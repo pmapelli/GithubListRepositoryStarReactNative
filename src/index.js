9ad39825a2c3ee05d7c4c2b79f9c3ca36dcfbfae
pmapelli <pmapelli@hotmail.com>
2019-10-07 10:50:23 -0300

    chore: Add Eslit Prettier EditorConfig

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>Bem vindo!</Text>
+      <Text style={styles.welcome}>Abaixo</Text>
+    </View>
+  );
+}
