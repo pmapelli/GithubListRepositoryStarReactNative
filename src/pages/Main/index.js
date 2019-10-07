b905fb3bb9bf3f8ff55656369f68766ed077b2fc
pmapelli <pmapelli@hotmail.com>
2019-10-07 17:33:06 -0300

    : Styling Listing

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -3,7 +3,19 @@ import { Keyboard } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import api from '../../services/api';
 
-import { Container, Form, SubmitButton, Input } from './styles';
+import {
+  Container,
+  Form,
+  SubmitButton,
+  Input,
+  List,
+  User,
+  Avatar,
+  Name,
+  Bio,
+  ProfileButton,
+  ProfileButtonText,
+} from './styles';
 
 export default class Main extends Component {
   state = {
@@ -32,7 +44,7 @@ export default class Main extends Component {
   };
 
   render() {
-    const { user, newUser } = this.state;
+    const { users, newUser } = this.state;
 
     return (
       <Container>
@@ -50,6 +62,22 @@ export default class Main extends Component {
             <Icon name="add" size={20} color="#FFF" />
           </SubmitButton>
         </Form>
+
+        <List
+          data={users}
+          keyExtractor={user => user.login}
+          renderItem={({ item }) => (
+            <User>
+              <Avatar source={{ uri: item.avatar }} />
+              <Name>{item.Name}</Name>
+              <Bio>{item.bio}</Bio>
+
+              <ProfileButton onPress={() => {}}>
+                <ProfileButtonText>Ver Perfil</ProfileButtonText>
+              </ProfileButton>
+            </User>
+          )}
+        />
       </Container>
     );
   }
